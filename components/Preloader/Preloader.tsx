7fa3687fc83287abd1fd1bcfc5b6046c7771ff73
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-03-12 10:13:56 +0530

    removed svg curve

--- components/Preloader/Preloader.tsx
+++ components/Preloader/Preloader.tsx
@@ -4,16 +4,16 @@ import { motion } from 'framer-motion';
 import { opacity, slideUp } from './animation';
 import { poppins } from '../../app/fonts/Fonts';
 
-const words = ["Hello","নমস্কার", "नमस्कार", "Bonjour", "Ciao", "Olà", "こんにちは", "Guten tag" ]
+const words = ["Hello","নমস্কার", "नमस्कार", "Bonjour", "Ciao", "Olà", "こんにちは", "Guten tag!" ]
 
 const Preloader: React.FC = () => {
 
     const [index, setIndex] = useState(0);
-    const [dimension, setDimension] = useState({width: 0, height:0});
+    //const [dimension, setDimension] = useState({width: 0, height:0});
 
-    useEffect( () => {
-        setDimension({width: window.innerWidth, height: window.innerHeight})
-    }, [])
+    // useEffect( () => {
+    //     setDimension({width: window.innerWidth, height: window.innerHeight})
+    // }, [])
 
     useEffect( () => {
         if(index == words.length - 1) return;
@@ -22,30 +22,34 @@ const Preloader: React.FC = () => {
         }, index == 0 ? 1000 : 150)
     }, [index])
 
-    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
-    const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`
+    // const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
+    // const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`
 
-    const curve = {
-        initial: {
-            d: initialPath,
-            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1]}
-        },
-        exit: {
-            d: targetPath,
-            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3}
-        }
-    }
+    // const curve = {
+    //     initial: {
+    //         d: initialPath,
+    //         transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1]}
+    //     },
+    //     exit: {
+    //         d: targetPath,
+    //         transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3}
+    //     }
+    // }
+
+    // {dimension.width > 0 && 
+    //     <>
+            
+    //         <svg className=' absolute top-0 w-full h-[calc(100%+300px)]'>
+    //             <motion.path variants={curve} initial="initial" exit="exit" className="fill-white"></motion.path>
+    //         </svg>
+    //     </>
+    // }
 
     return (
         <motion.div variants={slideUp} initial="initial" exit="exit" className=" h-screen w-screen flex items-center justify-center fixed z-1000 bg-white">
-            {dimension.width > 0 && 
-            <>
-                <motion.p variants={opacity} initial="initial" animate="enter" className={`flex text-black text-4xl items-center absolute z-10 ${poppins.className}`}><span className=' block w-3 h-3 bg-black mr-3 rounded-full'></span>{words[index]}</motion.p>
-                <svg className=' absolute top-0 w-full h-[calc(100%+300px)]'>
-                    <motion.path variants={curve} initial="initial" exit="exit" className="fill-white"></motion.path>
-                </svg>
-            </>
-            }
+            <motion.p variants={opacity} initial="initial" animate="enter" className={`flex text-black text-4xl items-center absolute z-10 ${poppins.className}`}>
+                <span className=' block w-3 h-3 bg-black mr-3 rounded-full'></span>{words[index]}
+            </motion.p>
         </motion.div>
     );
 }
