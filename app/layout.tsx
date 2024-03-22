cc7787e7ae2310c8c94fb0dc9c6ec7560845e7f1
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-03-22 19:47:33 +0530

    changes in og

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Soham Dutta • Full Stack Developer",
   description: "Full Stack Developer, Currently working at Nokia as an Intern in Bengaluru, India",
-};
+  openGraph:{
+    title: "Soham Dutta • Full Stack Developer",
+    description: "Full Stack Developer, Currently working at Nokia as an Intern in Bengaluru, India",
+    type: "website",
+    url: "https://portfolio-v2-xi-five.vercel.app/",
+  },
+  }
 
 export default function RootLayout({
   children,
