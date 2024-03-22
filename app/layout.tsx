980b7a716db7c8f3f26e8899df672733ecbaf763
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-03-22 17:09:39 +0530

    Added Social Media Preview Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Soham Dutta - Full Stack Developer",
   description: "Full Stack Developer, Currently working at Nokia in Bengaluru, India",
+  openGraph: {
+    title: 'Soham Dutta - Full Stack Developer',
+    description: 'Full Stack Developer, Currently working at Nokia in Bengaluru, India'
+}
 };
 
 export default function RootLayout({
