f8933f0b824cbc6d1133d3cf026a60ab650f64eb
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-03-22 20:22:58 +0530

    added type of og:image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: "Soham Dutta • Full Stack Developer",
-        type: "image/jpg",
+        type: "image/jpeg",
       },
     ],
 
