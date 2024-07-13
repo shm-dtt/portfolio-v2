47018c98b1c77f131f8df0540da6d3edae870b89
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-07-13 18:12:18 +0530

    Review changes

--- utils/data/Data.tsx
+++ utils/data/Data.tsx
@@ -21,21 +21,21 @@ export const Data = {
 export const testimonials = [
   {
     name: "Shawn D'souza",
-    role: "DevOps Engineer at Neighborly India, ex-SWE ZapCom Group lnc",
+    role: "DevOps Engineer at Neighborly India",
     statement:
       "A really good front end developer with great communication and team management skills. He is also a fast learner and adapts to various environments quickly.",
     color: "#191919",
   },
   {
     name: "Aniket Pathak",
-    role: "Ex-SDE Intern at Nokia",
+    role: "Software Engineer at Nokia",
     statement:
       "Soham and I worked together in the same team during our internship at Nokia. Being a great friend and a team player, he is one of the most knowledgeable people I have met in field of Full Stack Development with an eye for UI/UX and Frontend Design. A down to earth and humble guy, I have learned a lot working with him.",
     color: "#232323",
   },
   {
     name: "Sibananda Sahu",
-    role: "Software Engineer 1 at Synopsys Inc, Ex-Intern at Reliance Jio PLatforms Ltd",
+    role: "Software Engineer 1 at Synopsys Inc",
     statement:
       "Working with Soham has been an absolute pleasure! I can attest to his versatility and proficiency in web technologies. From UI/UX design to Spring Boot and React JS expertise, he consistently delivers top-notch solutions with precision and creativity. His attention to detail and innovative approach always ensure outstanding results.",
     color: "#2E2E2E",
