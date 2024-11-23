2729a9f0043e3b37d92782a243a56e42daa4c3e7
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-11-23 23:44:29 +0530

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Soham Dutta • Full Stack Developer",
@@ -32,6 +33,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
