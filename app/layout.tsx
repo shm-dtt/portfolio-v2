507a2af239a7122c0e2915affd9f035208c6646d
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-12-01 14:21:58 +0530

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Soham Dutta • Full Stack Developer",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
