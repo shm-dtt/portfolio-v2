0883e1397e593bdf52cd98ccc6ca810bca2307e9
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-12-03 18:55:31 +0530

    Removal of dot, replaced with -

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,11 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
-  title: "Soham Dutta • Full Stack Developer",
+  title: "Soham Dutta - Full Stack Developer",
   description:
     "I am a Full Stack Developer, Currently working at Nokia as an Applied R&D Software Engineer in Bengaluru, India",
   openGraph: {
-    title: "Soham Dutta • Full Stack Developer",
+    title: "Soham Dutta - Full Stack Developer",
     description:
       "I am a Full Stack Developer, Currently working at Nokia as an Applied R&D Software Engineer in Bengaluru, India",
     type: "website",
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
           "https://sohamdutta-portfolio.s3.ap-south-1.amazonaws.com/opengraph-image.jpg",
         width: 1200,
         height: 630,
-        alt: "Soham Dutta • Full Stack Developer",
+        alt: "Soham Dutta - Full Stack Developer",
         type: "image/jpeg",
       },
     ],
