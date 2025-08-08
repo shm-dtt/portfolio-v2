f167669001103151beb54d68ac99507ca6aacdca
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-08-08 15:48:31 +0530

    Blog page UI back to old UI

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,8 @@
 import { posts } from "#site/content";
 import { MDXContent } from "@/components/mdx-components";
+import ShareButton from "@/components/ui/ShareButton";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 interface PostPageProps {
   params: {
@@ -25,13 +28,43 @@ export async function generateStaticParams(): Promise<
 
 export default async function PostPage({ params }: PostPageProps) {
   const post = await getPostFromParams(params);
+  if (!post) {
+    return (
+      <div className="flex flex-col w-full h-full items-center justify-center">
+        <h1 className="text-2xl font-bold">Post not found</h1>
+        <Link
+          href="/blog"
+          className="my-8 flex flex-row text-sm items-center gap-2 text-neutral-400 hover:text-white"
+        >
+          <ChevronLeft size={16} /> Back
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div className="flex flex-col w-full h-full prose prose-invert dark:prose-invert max-w-3xl mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold">{post?.title}</h1>
-
-      <MDXContent code={post?.body || ""} />
-
-      {/* You can render the post content here once fetched */}
+    <div className="flex flex-col w-full h-full prose prose-invert dark:prose-invert">
+      <Link
+        href="/blog"
+        className="my-8 flex flex-row text-sm items-center gap-2 text-neutral-400 hover:text-white"
+      >
+        <ChevronLeft size={16} /> Back
+      </Link>
+      <h1 className="text-2xl font-bold">{post.title}</h1>
+      <div className="flex flex-row items-center justify-between gap-1 text-sm text-neutral-400">
+        <div className="flex flex-row items-center gap-2">
+          <p>
+            {new Date(post.date ?? "").toLocaleDateString("en-IN", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })}
+          </p>
+          •<p>{post.readingTime} min read</p>
+        </div>
+        <ShareButton title={post.title} link={post.slugAsParams} />
+      </div>
+      <hr className="mt-2 mb-8 border-neutral-600"></hr>
+      <MDXContent code={post.body || ""} />
     </div>
   );
 }
